d1ddb9e28d58d4425d66f55180f360e6c8ae4d4f
Bianca <biancamsoe@gmail.com>
2016-12-29 19:24:26 -0200

    Added update card quantity and level feature.

--- hello/views.py
+++ hello/views.py
@@ -13,6 +13,19 @@ def test(request):
   print request.POST
   return HttpResponse('<pre>' + 'hello' + '</pre>')
 
+def create_cards(request):
+  return render(request, 'create-cards.html')
+
+def dashboard(request):
+  handle = 'pepezineo'
+  user = User.objects.filter(handle=handle)
+  my_card_list = MyCard.objects.filter(user=user)
+  return render(request, 'dashboard.html', {'my_card_list': my_card_list})
+
+def db(request):
+  cards = Card.objects.all()
+  return render(request, 'db.html', {'cards': cards})
+
 def insert_cards_into_db(request):
   i = 0
   while 'name' + str(i) in request.POST:
@@ -28,7 +41,7 @@ def insert_cards_into_db(request):
     i += 1
   return HttpResponseRedirect('/')
 
-def insert_my_card_into_db(request):
+def insert_or_update_my_card_into_db(request):
   # TODO(bimaoe, catita): Remove this gambiarra when creation of users is 
   #     implemented.
   pepezineo = User(handle='pepezineo')
@@ -37,30 +50,21 @@ def insert_my_card_into_db(request):
   # TODO(bimaoe, catita): Remove this gambiarra when sessions are implemented.
   user = 'pepezineo'
   card = Card.objects.filter(name=request.POST['name'])[0]
-  
-  my_card = MyCard(user=pepezineo,
+  my_card = MyCard.objects.filter(user=pepezineo, card=card)
+  if my_card:
+    my_card.update(level=request.POST['level'], quantity=request.POST['quantity'])
+  else:
+    my_card = MyCard(user=pepezineo,
       card=card, 
       level=request.POST['level'], 
       quantity=request.POST['quantity'])
-  my_card.save()
+    my_card.save()
   return HttpResponseRedirect('/')
-
-def create_cards(request):
-  return render(request, 'create-cards.html')
-
-def db(request):
-  cards = Card.objects.all()
-  return render(request, 'db.html', {'cards': cards})
 
 def update_card(request):
   existing_cards = Card.objects.all()
   existing_cards = sorted(existing_cards, key=lambda x: x.name)
   return render(request, 'update-card.html', {'existing_cards': existing_cards})
 
-def dashboard(request):
-  handle = 'pepezineo'
-  user = User.objects.filter(handle=handle)
-  my_cards = MyCard.objects.filter(user=user)
-  my_card_list = [my_card.card for my_card in my_cards]
-  return render(request, 'dashboard.html', {'my_card_list': my_card_list})
+
 
